898e71b9620d33b75c52248476a4a1352eee18ee
heyshaquib <heyshaquib@users.noreply.github.com>
2026-08-28 02:20:32 +0530

    fix: attempt.... to fix suggestion notifications on my device

--- app/src/main/kotlin/org/koitharu/kotatsu/suggestions/ui/SuggestionsWorker.kt
+++ app/src/main/kotlin/org/koitharu/kotatsu/suggestions/ui/SuggestionsWorker.kt
@@ -411,7 +411,7 @@ class SuggestionsWorker @AssistedInject constructor(
 	) : PeriodicWorkScheduler {
 
 		override suspend fun schedule() {
-			val request = PeriodicWorkRequestBuilder<SuggestionsWorker>(24, TimeUnit.HOURS)
+			val request = PeriodicWorkRequestBuilder<SuggestionsWorker>(6, TimeUnit.HOURS)
 				.setConstraints(createConstraints())
 				.addTag(TAG)
 				.setBackoffCriteria(BackoffPolicy.LINEAR, 1, TimeUnit.HOURS)
